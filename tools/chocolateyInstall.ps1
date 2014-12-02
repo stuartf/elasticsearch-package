01947e5bc104c588d2f66f777ef4fa1c979f34c7
Richard Pijnenburg <richard@ispavailability.com>
2014-12-02 12:37:34 +0000

    Update ES to 1.4.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-Install-ChocolateyZipPackage 'elasticsearch' 'http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'elasticsearch' 'http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.1.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyPath "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\elasticsearch-1.2.1\bin"
+Install-ChocolateyPath "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\elasticsearch-1.4.1\bin"
