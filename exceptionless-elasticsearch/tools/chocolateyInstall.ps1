269d498052db0c3f50b879778723a1ab3dcbd09a
Chris S <mrshrinkray@gmail.com>
2016-02-10 19:30:27 +0000

    1.7.5 version as Exceptionless is still unsupported for 2.2

--- exceptionless-elasticsearch/tools/chocolateyInstall.ps1
+++ exceptionless-elasticsearch/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = "Stop";
 
 $packageName = "exceptionless-elasticsearch"
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checkSum = "79C18EC9AF1C3FC2EF0D168C23AB8A7696FCC123"
+$checkSum = "1099bc16a72ee20a5bfe3dd29eb3ca257ded7f66"
 
-$url = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.2.0/elasticsearch-2.2.0.zip"
+$url = "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.5.zip"
 
 Install-ChocolateyZipPackage "packageName" -url "$url" -unzipLocation "$toolsDir" -checksumType "sha1" -checksum "$checkSum"
